c432ec0cd3fad334a6dd56ab56f9889c81f7521c
Nick <nikitosi3012@gmail.com>
2025-11-26 02:11:57 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,43 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+        self._connection = sqlite3.connect(db_name)
+
+    def create(self, first_name: str, last_name: str) -> None:
+        cursor = self._connection.cursor()
+        cursor.execute(
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
+
+        self._connection.commit()
+
+    def all(self) -> list:
+        cursor = self._connection.cursor()
+        instances = cursor.execute(f"SELECT * FROM {self.table_name}")
+        rows = instances.fetchall()
+
+        return [Actor(*row) for row in rows]
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        cursor = self._connection.cursor()
+        cursor.execute(
+            f"""
+            UPDATE {self.table_name} 
+            SET first_name = ?, last_name = ? 
+            WHERE id = ? 
+            """,
+            (new_first_name, new_last_name, pk)
+        )
+        self._connection.commit()
+
+    def delete(self, pk: int) -> None:
+        cursor = self._connection.cursor()
+        cursor.execute(f"DELETE FROM {self.table_name} WHERE id = ? ", (pk,))
+
+        self._connection.commit()
